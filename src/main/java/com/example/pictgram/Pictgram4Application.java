78bb09604cef079f6bb74d405f781752b1963d20
mana <wasemikanm@yahoo.co.jp>
2024-10-08 11:00:26 +0900

    first commit

---
+++ src/main/java/com/example/pictgram/Pictgram4Application.java
@@ -0,0 +1,13 @@
+package com.example.pictgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pictgram4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pictgram4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pictgram/Pictgram4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pictgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pictgram4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
